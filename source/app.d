9c6c319b1fb73b68c93bc477bfc5222c3f899696
Casey Sybrandy <casey.sybrandy@six3systems.com>
2016-03-06 21:21:23 -0500

    Added command line options.
    Also, added a dummy test to ensure unit-threaded does pass until actual tests
    can be written.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,45 @@
+import std.getopt;
 import std.stdio;
 
-void main()
+struct Options
 {
+    string[] stats;
+    int[] percentiles;
+    int threads;
+}
+
+void main(string[] args)
+{
+    Options opts = getOptions(args);
+}
+
+Options getOptions(string[] args)
+{
+    immutable bool[string] supportedStats = [
+        "count": true,
+        "sum": true,
+        "mean": true,
+        "median": true,
+        "mode": true,
+        "stddev": true,
+        "range": true,
+        "percentiles": true
+    ];
+    string percentiles, stats;
+    int threads;
+    Options currOptions;
+
+    auto help = getopt(args,
+        "percentiles|p", "A comma separated list of percentiles", &percentiles,
+        "stats|s", "A comma separated list of statistical measures", &stats,
+        "threads|t", "The number of worker threads to use", &threads
+    );
+
+    if (help.helpWanted)
+    {
+        defaultGetoptPrinter("pst - Pipable Statistics", help.options);
+        writeln("Supported statistical measures: ", supportedStats.keys);
+    }
+
+    return currOptions;
 }
---
+++ source/stats.d
@@ -0,0 +1,5 @@
+@("Some test")
+unittest
+{
+    assert(true);
+}
