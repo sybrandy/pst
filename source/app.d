8ce6e82ae721f5b6869396a144200a2d18c1a60a
Casey Sybrandy <casey.sybrandy@six3systems.com>
2016-03-19 15:33:41 -0400

    Added initial code to set up the runner.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,5 @@
-import std.getopt;
-import std.stdio;
+import std.array, std.algorithm, std.getopt, std.stdio, std.string, std.conv;
+import runner.fiber;
 
 struct Options
 {
@@ -8,9 +8,31 @@ struct Options
     int threads;
 }
 
-void main(string[] args)
+int main(string[] args)
 {
     Options opts = getOptions(args);
+    bool isFloat = false;
+
+    if (opts.stats.length == 0)
+    {
+        writeln("You must specify at least one stat to calculate.");
+        return 1;
+    }
+    foreach (string line; stdin.byLineCopy)
+    {
+        if (!isNumeric(line))
+        {
+            writeln("The value must be a number.");
+            return 1;
+        }
+        else if (indexOf(line, '.') > -1)
+        {
+            isFloat = true;
+        }
+        writeln("Line: ", line);
+        writeln("Is a float: ", isFloat);
+    }
+    return 0;
 }
 
 Options getOptions(string[] args)
@@ -26,7 +48,7 @@ Options getOptions(string[] args)
         "percentiles": true
     ];
     string percentiles, stats;
-    int threads;
+    int threads = 0;
     Options currOptions;
 
     auto help = getopt(args,
@@ -34,6 +56,10 @@ Options getOptions(string[] args)
         "stats|s", "A comma separated list of statistical measures", &stats,
         "threads|t", "The number of worker threads to use", &threads
     );
+
+    currOptions.stats = stats.split(",");
+    currOptions.percentiles = percentiles.split(",").map!(to!int).array();
+    currOptions.threads = threads;
 
     if (help.helpWanted)
     {
